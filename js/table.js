6a897243b3715d93d8c54b57edac414a85c6369d
Jason Downing <jason_downing@student.uml.edu>
2015-10-15 15:22:55 -0400

    table is good I think. error checking time.

--- js/table.js
+++ js/table.js
@@ -6,7 +6,7 @@
     MIT Licensed - see http://opensource.org/licenses/MIT for details.
     Anyone may freely use this code. Just don't sue me if it breaks stuff.
     Created: Oct 13, 2015.
-    Last Updated: Oct 13, 9PM.
+    Last Updated: Oct 15th, 3:30PM
 
     This page is for the 6th assignment, "Creating an Interactive Dynamic Table".
     It contains a JavaScript function called table_calc() which calculates out
@@ -16,15 +16,6 @@
 
 // This function calculates the multiplication table.
 function table_calc() {
-  // Calculate the multiplication table.
-  // We'll do a 2d array, of:
-  /*
-      x 1  2  3  4  5
-      5 5 10 15 20 25
-      6 6 12 18 24 30
-      etc
-  */
-
   // User input
   var hor_start = document.getElementById('horiz_start').value;
   var hor_end = document.getElementById('horiz_end').value;
@@ -36,28 +27,60 @@ function table_calc() {
     return;   // Can't do this.
   }
 
-  // Found how to do this on Stackoverflow
-  // URL: https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript
   var calc = [];
 
-  for (var x = 0; x <= hor_end; x++) {   // x < Ending length
+  /*
+    The 2D array works like this.
+    Calc is an array, which will contain many more arrays. The main array,
+    or the first bracket pair ([here][]), will control how far horizontally
+    the table goes. Each position in this array will then contain another array,
+    which contains all the vertical positions for that column.
+
+    x 1 2 3  4  5   -> each of these positions is an array, contained inside array "calc"
+    1 1 2 3  4  5
+    2 2 4 8 10 12
+    3 3 6 9 12 15
+    ^ ->  each vertical position is a location inside of the vertical array
+          for this column.
+
+    Example for horizontal 1 to 5, and vertical 1 to 5:
+
+         Horizontally there are 5 vertical arrays.
+    [ Array[5], Array[5], Array[5], Array[5], Array[5] ]
+
+    The first vertical Array[5] is: [1, 2, 3]
+    The second one would be: [2, 4, 6]
+    etc.
+
+    I found how to do this on Stackoverflow
+    https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript
+  */
+  for (var x = 0; x <= hor_end - hor_start; x++) {   // x <= horizontal length
     calc[x] = [];
   }
 
-  var hor = 0;          // Indexes for the 2D array.
+  // Indexes for the 2D array.
+  var hor = 0;
   var vert = 0;
 
-  // Now let's calculate the multiplication table using a 2D array.
-  // I figured out how to do this using this stackoverflow post:
-  // https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript
-  //         vert beg  vert ending
+  /*  Now let's calculate the multiplication table using a 2D array.
+      This parts confusing so let me explain:
+      I start at the first vertical position, and then calculate out that
+      entire row. I do a similar for loop in the table_fill to access each
+      row one at a time so that the table's <tr> tags have an entire row of
+      multiplication data.
+      I used indexes that start at 0 since the 2D array starts at 0 as well.
+      The two for loops can then be used for multiplying numbers.
+      The vert position index increases each time as we move down a row.
+      The horizontal index resets to zero each time since we're doing a
+  */
   for (var x = vert_start; x <= vert_end; x++) {
-    //       hor beg  hor ending
     for (var y = hor_start; y <= hor_end; y++) {
-      calc[hor][vert] = x * y;    // Calculate the given spot in the multiplication table.
-      hor++;                      // Horizontal counter increments each time.
+      // Calculate the given spot in the multiplication table.
+      calc[hor][vert] = x * y;
+      hor++;
     }
-    hor = 0;
+    hor = 0;        // Reset to zero each pass since we're moving down a row.
     vert++;
   }
 
@@ -82,12 +105,12 @@ function table_fill(calc_array) {
   // Opening table tags.
   content += "<table>";
 
-  // Start by putting the empty spot in the top left corner.
-  content += "<tr class='dyn_tr_td'><td></td>";
+  // First row, and put an empty spot in the top left corner.
+  content += "<tr><td></td>";
 
   // Now fill out the rest of the first row.
   for (var x = hor_start; x <= hor_end; x++) {
-    content += "<td class='dyn_tr_td'>" + x + "</td>";
+    content += "<td>" + x + "</td>";
   }
 
   // Close the first row.
@@ -98,12 +121,15 @@ function table_fill(calc_array) {
 
   // Fill in each row after the first.
   for (var x = vert_start; x <= vert_end; x++) {
-    content += "<tr class='dyn_tr_td'> <td class='dyn_tr_td'>" + x + "</td>";
+    // Set the left most column first.
+    content += "<tr><td>" + x + "</td>";
 
+    // Add in all the multiplication for this row.
     for (var y = hor_start; y <= hor_end; y++) {
-      content += "<td class='dyn_tr_td'>" + calc_array[hor][vert] + "</td>";
+      content += "<td>" + calc_array[hor][vert] + "</td>";
       hor++;
     }
+    // Reset horizontal counter each time as we move down a row.
     hor = 0;
     vert++;
 
