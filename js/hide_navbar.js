de10d2556b4ff5b66ddc42dcbcd9eefd81874fa1
Jason Downing <jdown1994@gmail.com>
2015-11-24 18:51:04 -0500

    got one line of scrabble to show up and play with

--- js/hide_navbar.js
+++ js/hide_navbar.js
@@ -1,5 +1,5 @@
 /*
-    File: ~/js/scrabble.js
+    File: ~/js/hide_navbar.js
     91.461 Assignment 9: Implementing a Bit of Scrabble with Drag-and-Drop
     Jason Downing - student at UMass Lowell in 91.461 GUI Programming I
     Contact: jdowning@cs.uml.edu or jason_downing@student.uml.edu
@@ -9,10 +9,13 @@
     Last Updated: Nov 24, 1:30PM.
 
     This JavaScript file is for the 9th assignment, "Scrabble".
+
+    I found this on Stackoverflow. It is handy for making better use of
+    the screen, since you really don't need to see the Nav bar when playing Scrabble.
+
+    URL: https://stackoverflow.com/questions/23315031/how-to-create-a-hidden-navbar-with-bootstrap-that-shows-after-you-scroll
 */
-
-function load_scrabble_pieces() {
-  var pieces = pieces.json;
-
-  //var random
+function hide_navbar() {
+  // hide .navbar first
+  $(".navbar").hide();
 }
