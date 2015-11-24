c4caf867022a813fc2fe8b0c83a0a6e90270b6a1
Jason Downing <jason_downing@student.uml.edu>
2015-11-24 13:41:57 -0500

    all the scrabble images! thanks Prof. Jesses!

---
+++ js/scrabble.js
@@ -0,0 +1,13 @@
+/*
+    File: ~/js/scrabble.js
+    91.461 Assignment 9: Implementing a Bit of Scrabble with Drag-and-Drop
+    Jason Downing - student at UMass Lowell in 91.461 GUI Programming I
+    Contact: jdowning@cs.uml.edu or jason_downing@student.uml.edu
+    MIT Licensed - see http://opensource.org/licenses/MIT for details.
+    Anyone may freely use this code. Just don't sue me if it breaks stuff.
+    Created: Nov 24, 2015.
+    Last Updated: Nov 24, 1:30PM.
+
+    This JavaScript file is for the 9th assignment, "Scrabble".
+*/
+
