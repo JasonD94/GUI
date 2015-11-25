b7aef531850c5ad0f92279bccbe96a307463eeb2
Jason Downing <jdown1994@gmail.com>
2015-11-24 22:44:33 -0500

    making some good progress on scoring and what not.

--- js/scrabble.js
+++ js/scrabble.js
@@ -12,6 +12,133 @@
 */
 
 /**
+ *    Global variables for ease of use. Lazy lazy lazy but whatever.
+ */
+
+// JavaScript array of objects for the amounts and value of each letter.
+// I didn't make this data structure, this was originally found on Piazza and made by Ramon Meza.
+// Also, I didn't feel like figuring out how to load a JSON file again so I did the easy way
+// and just made a pieces array with all the stuff I need. Obviously lazy but it works way easier SO WHY NOT.
+var pieces = [
+  {"letter":"A", "value":1,  "amount":9},
+  {"letter":"B", "value":3,  "amount":2},
+  {"letter":"C", "value":3,  "amount":2},
+  {"letter":"D", "value":2,  "amount":4},
+  {"letter":"E", "value":1,  "amount":12},
+  {"letter":"F", "value":4,  "amount":2},
+  {"letter":"G", "value":2,  "amount":3},
+  {"letter":"H", "value":4,  "amount":2},
+  {"letter":"I", "value":1,  "amount":9},
+  {"letter":"J", "value":8,  "amount":1},
+  {"letter":"K", "value":5,  "amount":1},
+  {"letter":"L", "value":1,  "amount":4},
+  {"letter":"M", "value":3,  "amount":2},
+  {"letter":"N", "value":1,  "amount":6},
+  {"letter":"O", "value":1,  "amount":8},
+  {"letter":"P", "value":3,  "amount":2},
+  {"letter":"Q", "value":10, "amount":1},
+  {"letter":"R", "value":1,  "amount":6},
+  {"letter":"S", "value":1,  "amount":4},
+  {"letter":"T", "value":1,  "amount":6},
+  {"letter":"U", "value":1,  "amount":4},
+  {"letter":"V", "value":4,  "amount":2},
+  {"letter":"W", "value":4,  "amount":2},
+  {"letter":"X", "value":8,  "amount":1},
+  {"letter":"Y", "value":4,  "amount":2},
+  {"letter":"Z", "value":10, "amount":1},
+  {"letter":"_", "value":0,  "amount":2}
+];
+
+// JavaScript array of objects to determine what letter each piece is.
+var game_tiles = [
+  {"id": "piece0", "letter": "A"},
+  {"id": "piece1", "letter": "B"},
+  {"id": "piece2", "letter": "C"},
+  {"id": "piece3", "letter": "D"},
+  {"id": "piece4", "letter": "E"},
+  {"id": "piece5", "letter": "F"},
+  {"id": "piece6", "letter": "G"}
+]
+
+// JavaScript object to keep track of the game board.
+// NOTE: "pieceX" means NO tile present on that drop zone.
+var game_board = [
+  {"id": "drop1",  "tile": "pieceX"},
+  {"id": "drop2",  "tile": "pieceX"},
+  {"id": "drop3",  "tile": "pieceX"},
+  {"id": "drop4",  "tile": "pieceX"},
+  {"id": "drop5",  "tile": "pieceX"},
+  {"id": "drop6",  "tile": "pieceX"},
+  {"id": "drop7",  "tile": "pieceX"},
+  {"id": "drop8",  "tile": "pieceX"},
+  {"id": "drop9",  "tile": "pieceX"},
+  {"id": "drop10", "tile": "pieceX"},
+  {"id": "drop11", "tile": "pieceX"},
+  {"id": "drop12", "tile": "pieceX"},
+  {"id": "drop13", "tile": "pieceX"},
+  {"id": "drop14", "tile": "pieceX"},
+  {"id": "drop15", "tile": "pieceX"}
+]
+
+
+/**
+ *    When called, this function determine what the current word is, and prints it
+ *    out to the HTML doc as well as the console for logging purposes.
+ *
+ */
+function find_word() {
+
+}
+
+
+/**
+ *    This function, given a letter, will return the letter's score based on
+ *    the value in the pieces.json file.
+ *
+ *    parameters: a letter, such as "A", "B", "C", etc.
+ *    returns: integer score, such as "1" or "2". On error, returns "-1"
+ */
+function find_score(given_id) {
+  // First figure out which letter we have.
+  var letter = find_letter(given_id);
+
+  // Since each "letter" is actually a spot in an array in the pieces.json file,
+  // we gotta look at each object in the array before we can look at stuff.
+  for(var i = 0; i < 27; i++) {
+    // Get an object to look at.
+    var obj = pieces[i];
+
+    // See if this is the right object.
+    if(obj.letter == letter) {
+      //console.log("we got letter: " + obj.letter);
+      //console.log("value of letter is: " + obj.value);
+      return obj.value;
+    }
+  }
+
+  // If we get here, then we weren't given a nice valid letter. >:(
+  return -1;
+}
+
+
+/**
+ *    This function, given a piece ID will return which letter it represents.
+ *    Ex: given piece1, returns "A".
+ */
+function find_letter(given_id) {
+  // Go through the 7 pieces,
+  for(var i = 0; i < 7; i++) {
+    // If we found the piece we're looking for, awesome!
+    if(game_tiles[i].id == given_id) {
+      // Just return its letter!
+      return game_tiles[i].letter;
+    }
+  }
+
+  // If we get here, we weren't given a nice draggable ID like "piece1"
+}
+
+/**
  *    This function loads up the scrabble pieces onto the rack.
  *    It also makes each of them draggable and sets various properties, including
  *    the images location and class / ID.
@@ -20,22 +147,23 @@
  *
  */
 function load_scrabble_pieces() {
+  // I'm so used to C++ that I like defining variables at the top of a function. *shrugs*
   var base_url = "img/scrabble/Scrabble_Tile_";   // base URL of the image
-  var tiles = [ "A", "B", "C", "Blank", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-                "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var random_num = 1;
   var piece = "<img class='pieces' id='piece" + i + "' src='" + base_url + random_num + ".jpg" + "'></img>";
   var piece_ID = "";
+  var what_piece = "";
 
   // Load up 7 pieces
-  for(var i = 1; i < 8; i++) {
+  for(var i = 0; i < 7; i++) {
     // Get a random number so we can generate a random tile. There's 27 tiles,
     // so we want a range of 0 to 26.
     random_num = getRandomInt(0, 26);
 
     // Make the img HTML and img ID so we can easily append the tiles.
-    piece = "<img class='pieces' id='piece" + i + "' src='" + base_url + tiles[random_num] + ".jpg" + "'></img>";
+    piece = "<img class='pieces' id='piece" + i + "' src='" + base_url + pieces[random_num].letter + ".jpg" + "'></img>";
     piece_ID = "#piece" + i;
+    game_tiles[i].letter = pieces[random_num].letter;
 
     // Reposition the tile on top of the rack, nicely in a row with the other tiles.
 
@@ -69,6 +197,7 @@ function load_scrabble_pieces() {
     $(piece_ID).draggable();
   }
 }
+
 
 /**
  *    This function will load up targets for the images to be dropped onto.
@@ -114,7 +243,15 @@ function load_droppable_targets() {
         // https://stackoverflow.com/questions/5562853/jquery-ui-get-id-of-droppable-element-when-dropped-an-item
         var draggableID = ui.draggable.attr("id");
         var droppableID = $(this).attr("id");
-        $("#score").html("Tile: " + draggableID + " - dropped on " + droppableID);
+
+        // Log this stuff for debugging.
+        console.log("Tile: " + draggableID + " - dropped on " + droppableID);
+
+        // Mark that a tile was dropped in the game_board variable.
+
+
+        // Put the score of the dropped tile into the HTML doc.
+        $("#score").html(find_score(draggableID));
       }
     });
   }
