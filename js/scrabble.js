aa8542efb9c6304a561d490bc33f2458dae3d119
Jason Downing <jdown1994@gmail.com>
2015-11-24 19:25:59 -0500

    oh yeah, 7 randomish tiles on the screen. BUT WHY AREN'T THEY DRAGABLE? THE WORLD WILL NEVER KNOW.

--- js/scrabble.js
+++ js/scrabble.js
@@ -12,7 +12,27 @@
 */
 
 function load_scrabble_pieces() {
-  var pieces = pieces.json;
+  var base_url = "img/scrabble/Scrabble_Tile_";   // base URL of the image
+  var tiles = [ "A", "B", "C", "Blank", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+                "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var random_num = 1;
+  var piece = "<img class='pieces' id='piece" + i + "' src='" + base_url + random_num + ".jpg" + "'></img>";
 
-  //var random
+  // Load up 7 pieces
+  for(var i = 1; i < 8; i++) {
+    random_num = getRandomInt(0, 26);
+    piece = "<img class='pieces' id='piece" + i + "' src='" + base_url + tiles[random_num] + ".jpg" + "'></img>";
+    $("#rack").append(piece);
+  }
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ *
+ * I did not originally write this, it is from this Stackoverflow post:
+ * URL: https://stackoverflow.com/questions/1527803/generating-random-numbers-in-javascript-in-a-specific-range
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
